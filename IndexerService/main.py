1e279809597fa5216b2093b3295abf528b9af430
Benyamin Shoraki <benyaminchor@gmail.com>
2026-02-26 17:07:10 +0200

    Add Indexer Service: implement Docker setup and Kafka message consumer

---
+++ IndexerService/main.py
@@ -0,0 +1,34 @@
+import json
+from Shared.config import settings
+from Shared.kafka_connection import kafka_service
+from Shared.logger_config import get_logger
+from IndexerService.utils import process_message
+
+logger = get_logger("indexer-service")
+
+def start_consumer():
+    kafka_service.consumer.subscribe([settings.CONSUME_TOPIC])
+    logger.info(f"Indexer Service started. Listening to topic: {settings.CONSUME_TOPIC}")
+
+    try:
+        while True:
+            msg = kafka_service.consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                logger.error(f"Kafka error: {msg.error()}")
+                continue
+
+            try:
+                msg_value = json.loads(msg.value().decode('utf-8'))
+                process_message(msg_value)
+            except Exception as e:
+                logger.error(f"Failed to process message: {e}")
+
+    except KeyboardInterrupt:
+        logger.info("Shutting down Indexer service...")
+    finally:
+        kafka_service.consumer.close()
+
+if __name__ == "__main__":
+    start_consumer()
