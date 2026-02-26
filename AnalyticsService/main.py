60bd53f2b7b564711884d2f35da4040af72631d1
Benyamin Shoraki <benyaminchor@gmail.com>
2026-02-26 16:40:18 +0200

    Add Analytics Service: implement Kafka consumer and message processing logic

---
+++ AnalyticsService/main.py
@@ -0,0 +1,34 @@
+import json
+from shared.config import settings
+from shared.kafka_connection import kafka_service
+from shared.logger_config import get_logger
+from AnalyticsService.utils import process_message
+
+logger = get_logger("analytics-service")
+
+def start_consumer():
+    kafka_service.consumer.subscribe([settings.CONSUME_TOPIC])
+    logger.info(f"Analytics Service started. Listening to topic: {settings.CONSUME_TOPIC}")
+
+    try:
+        while True:
+            msg = kafka_service.consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                logger.error(f"Kafka error: {msg.error()}")
+                continue
+
+            try:
+                msg_value = json.loads(msg.value().decode('utf-8'))
+                process_message(msg_value)
+            except Exception as e:
+                logger.error(f"Failed to process message: {e}")
+
+    except KeyboardInterrupt:
+        logger.info("Shutting down Analytics service...")
+    finally:
+        kafka_service.consumer.close()
+
+if __name__ == "__main__":
+    start_consumer()
